d3167f2fa4acd28ce914eeed18a9d9b2a890b338
BruceChaun <guiqicj@gmail.com>
2017-04-05 16:37:20 -0400

    load module and setting

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,11 @@
+module purge 
+module load python/gnu/3.4.4
+export PYSPARK_PYTHON=/share/apps/python/3.4.4/bin/python
+export PYTHONHASHSEED=0
+export SPARK_YARN_USER_ENV=PYTHONHASHSEED=0
+
 out="$1"
 rm "$out.txt"
-spark-submit "$out.py" /user/jc7459/small_311.csv
+spark-submit "$out.py" /user/jc7459/311.csv
 hadoop fs -getmerge "$out.out" "$out.txt"
 hadoop fs -rm -r "$out.out"
--- src/run_all.sh
+++ src/run_all.sh
@@ -1,11 +1,17 @@
-#./run.sh complaint_type
-#./run.sh location_type
-#./run.sh nums_in_month
-#./run.sh city
-#./run.sh agency
-#./run.sh duration_by_agency
-#./run.sh zipcode
-#./run.sh nums_in_hour
-#./run.sh nums_in_month_by_agency
-#./run.sh nums_in_hour_by_agency
+module purge 
+module load python/gnu/3.4.4
+export PYSPARK_PYTHON=/share/apps/python/3.4.4/bin/python
+export PYTHONHASHSEED=0
+export SPARK_YARN_USER_ENV=PYTHONHASHSEED=0
+
+./run.sh complaint_type
+./run.sh location_type
+./run.sh nums_in_month
+./run.sh city
+./run.sh agency
+./run.sh duration_by_agency
+./run.sh zipcode
+./run.sh nums_in_hour
+./run.sh nums_in_month_by_agency
+./run.sh nums_in_hour_by_agency
 ./run.sh nums_in_month_by_agency_year
