fa3105980182469a01764a5bb47c591b0c10c434
cipta <ciphwn@gmail.com>
2017-04-05 00:06:29 -0400

    extraction

--- process_all_dates.py
+++ process_all_dates.py
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Process all date columns.
-process_dates.sh extract_yellow_small.csv 5
-process_dates.sh extract_yellow_2.csv 20
+./process_dates.sh extract_yellow_small.csv 5
+./process_dates.sh extract_yellow_2.csv 20
