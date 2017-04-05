6af1e2731c59fdbe025d24ef62affc60bf206d96
BruceChaun <guiqicj@gmail.com>
2017-04-05 16:13:17 -0400

    use python 3.4.4 on server, refine auto script

--- src/run_all.sh
+++ src/run_all.sh
@@ -1,3 +1,9 @@
+module purge 
+module load python/gnu/3.4.4
+export PYSPARK_PYTHON=/share/apps/python/3.4.4/bin/python
+export PYTHONHASHSEED=0
+export SPARK_YARN_USER_ENV=PYTHONHASHSEED=0
+
 #./run.sh complaint_type
 #./run.sh location_type
 #./run.sh nums_in_month
