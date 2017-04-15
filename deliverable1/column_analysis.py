a710babf591e0dfdca54a4ff4ad1bcdeb89d8aff
Alex Takata <takata.alex@gmail.com>
2017-04-15 15:59:19 -0400

    0, 3-6, 9 columns

--- deliverable1/column_analysis.py
+++ deliverable1/column_analysis.py
@@ -16,13 +16,131 @@ def to_csv(l):
     return ','.join(l)
 
 def parse_0_vendor(x):
-    pass
+    key, occur_count = x
+    base_type = 'INT'
+    semantic_type = 'vendor id'
+    data_label = 'VALID'  #VALID | NULL | INVALID
+    if key is None:
+        base_type = 'NULL'
+        semantic_type = 'unknown value'
+        data_label = 'NULL'
+    else:
+        try:
+            int(key)
+            if key not in ('1', '2'):
+                data_label = 'INVALID'
+        except:
+            base_type = 'STRING'
+            semantic_type = 'unknown value'
+            data_label = 'INVALID'
+    return (key, base_type, semantic_type, data_label, occur_count)
+
 
 def parse_3_passenger_count(x):
-    pass
+    key, occur_count = x
+    base_type, semantic_type, data_label = 'INT', 'passenger count', 'VALID'
+    if key is None:
+        base_type = 'NULL'
+        semantic_type = 'missing value'
+        data_label = 'NULL'
+    else:
+        try:
+            int(key)
+            if (int(key) > 6) or (int(key) <= 0):
+                data_label = 'INVALID|OUTLIER'
+        except:
+            base_type = 'STRING'
+            semantic_type = 'unknown value'
+            data_label = 'INVALID'
+    return (key, base_type, semantic_type, data_label, occur_count)
+
 
 def parse_4_trip_distance(x):
-    pass
+    key, occur_count = x
+    base_type, semantic_type, data_label = 'FLOAT', 'distance (miles)', 'VALID'
+    if key is None:
+        base_type = 'NULL'
+        semantic_type = 'missing value'
+        data_label = 'NULL'
+    else:
+        try:
+            float(key)
+#***TO CHECK
+            if (float(key) <= 0.0) or (float(key) >= 100.0):
+                semantic_type = 'INVALID|OUTLIER'
+        except:
+            base_type = 'STRING'
+            semantic_type = 'unknown value'
+            data_label = 'INVALID'
+    return (key, base_type, semantic_type, data_label, occur_count)
+
+
+def parse_5_rate_code(x):
+    key, occur_count = x
+    base_type, semantic_type, data_label = 'INT', 'rate code id', 'VALID'
+    if key is None:
+        base_type, semantic_type, data_label = 'NULL', 'missing value', 'NULL'
+    else:
+        try:
+            int(key)
+            if key not in ('1','2','3','4','5','6'):
+                data_label = 'INVALID'
+            elif key == '99':
+                data_label = 'NULL'
+        except:
+            base_type = 'STRING'
+            semantic_type = 'unknown value'
+            data_label = 'INVALID'
+    return (key, base_type, semantic_type, data_label, occur_count)
+
+
+def parse_6_store_and_fwd(x):
+    key, occur_count = x
+    base_type, semantic_type, data_label = 'BOOLEAN', 'store and forward flag', 'VALID'
+    if key is None:
+        base_type, semantic_type, data_label = 'NULL', 'missing value', 'NULL'
+    else:
+        if key not in ('Y', 'N'):
+            data_label, base_type, semantic_type = 'INVALID', 'STRING', 'unknown value'
+    return (key, base_type, semantic_type, data_label, occur_count)
+
+
+def parse_9_payment_type(x):
+    key, occur_count = x
+    base_type, semantic_type, data_label = 'INT', 'payment_type', 'VALID'
+    if key is None:
+        base_type, semantic_type, data_label = 'NULL', 'missing value', 'NULL'
+    else:
+        try:
+            int(key)
+            if key not in ('1','2','3','4','5','6'):
+                data_label = 'INVALID'
+        except:
+            base_type, semantic_type, data_label = 'STRING', 'unknown value', 'INVALID'
+    return (key, base_type, semantic_type, data_label, occur_count)
+
+
+def parse_10_fare(x):
+    key, occur_count = x
+    base_type, semantic_type, data_label = 'FLOAT', 'fare amount (Dollars)', 'VALID'
+    if key is None:
+        base_type = 'NULL'
+        semantic_type = 'missing value'
+        data_label = 'NULL'
+    else:
+        try:
+            float(key)
+            key = key // 10 * 10
+#***TO CHECK
+            if (float(key) <= 0.0) or (float(key) >= 100.0):
+                semantic_type = 'INVALID|OUTLIER'
+        except:
+            base_type = 'STRING'
+            semantic_type = 'unknown value'
+            data_label = 'INVALID'
+    # return (key, base_type, semantic_type, data_label, occur_count)
+    return (key, occur_count)
+
 
 
 def main():
@@ -31,7 +149,7 @@ def main():
                     # 'tpep_dropoff_datetime': parse_2,
                     # 'passenger_count': parse_3_passenger_count,
                     # 'trip_distance': parse_4_trip_distance,
-                    # 'RatecodeID': parse_5,
+                    # 'RatecodeID': parse_5_rate_code,
                     # 'store_and_fwd_flag': parse_6,
                     # 'PULocationID': parse_7,
                     # 'DOLocationID': parse_8,
@@ -47,13 +165,15 @@ def main():
 
     sc = SparkContext()
     rdd = sc.textFile(file_path, minPartitions = min_partitions)
+    header = rdd.first() #extract header
+    rdd = rdd.filter(lambda row: row != header)
     for i, (col, parse_func) in enumerate(column_dict.iteritems()):
         values = rdd.map(lambda x: (next(csv.reader([x]))[i], 1)).\
                     reduceByKey(add)
         values = values.map(parse_func)
         values.map(to_csv).saveAsTextFile(save_path + '_{}.csv'.format(col))
 
-        
+
 
 if __name__ == '__main__':
     main()
