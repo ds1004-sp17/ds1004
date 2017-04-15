b89f04f1ad3abd19a95a87afe891ab3801e1eaf1
cipta <ciphwn@gmail.com>
2017-04-15 18:49:59 -0400

    fix bugs

--- deliverable1/column_analysis.py
+++ deliverable1/column_analysis.py
@@ -189,17 +189,25 @@ def parse_10_fare(x):
 ################################################################################
 
 def keep_valid(row):
-    # Row: (value, base_type, semantic_type, valid_invalid)
-    if row[3] == 'VALID' and random.random() > args.keep_valid_rate:
-        return False
-    return True
+    try:
+        # Row: (value, base_type, semantic_type, valid_invalid)
+        if row[3] == 'VALID' and random.random() > args.keep_valid_rate:
+            return False
+        return True
+    except:
+        return True
+
+def csv_row_read(x):
+    return next(csv.reader([x]))
 
 def main():
     # Keep as a list because dictionary iteration order is undefined.
-    column_dict = [
-        ('VendorID', parse_0_vendor),
-        ('tpep_pickup_datetime', parse_1_pickup_datetime),
-        ('tpep_dropoff_datetime', parse_2_dropoff_datetime),
+    column_dict = {
+        'VendorID': parse_0_vendor,
+        'pickup_datetime': parse_1_pickup_datetime,
+        'dropoff_datetime': parse_2_dropoff_datetime,
+        'tpep_pickup_datetime': parse_1_pickup_datetime,
+        'tpep_dropoff_datetime': parse_2_dropoff_datetime,
         # 'passenger_count': parse_3_passenger_count,
         # 'trip_distance': parse_4_trip_distance,
         # 'RatecodeID': parse_5,
@@ -214,7 +222,7 @@ def main():
         # 'tolls_amount': parse_14,
         # 'improvement_surcharge': parse_15,
         # 'total_amount': parse_16
-    ]
+    }
 
     sc = SparkContext()
 
@@ -222,11 +230,16 @@ def main():
     sc.addPyFile('datetimes.py')
 
     rdd = sc.textFile(args.file_path, minPartitions=args.min_partitions)
-    header = rdd.first() #extract header
+    header = csv_row_read(rdd.first()) #extract header
     rdd = rdd.filter(lambda row: row != header)
-    for i, (col, parse_func) in enumerate(column_dict):
+    for i, col in enumerate(header):
+        parse_func = column_dict.get(col, None)
+        if parse_func is None:
+            print('Don\'t know how to read column: {0}'.format(col))
+            continue
+
         # Get unique values and their counts.
-        values = rdd.map(lambda x: (next(csv.reader([x]))[i], 1)).\
+        values = rdd.map(lambda x: (csv_row_read(x)[i], 1)).\
                     reduceByKey(add)
         # Feed values RDD to a parser.
         values = values.map(parse_func)
