61d6938766931172a6fd87f4129da70258ce7beb
xiaodiu2010 <xiaodiu2010@gmail.com>
2017-04-16 16:39:29 -0400

    analysis

--- src/analysis/type_year_pie.py
+++ src/analysis/type_year_pie.py
@@ -1,10 +1,9 @@
 from csv import reader
 from pyspark import SparkContext
 from operator import add
-import pandas as pd
 import sys
 import os
-#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 from utils import sum_none, check_datetime, get_date
 
 
