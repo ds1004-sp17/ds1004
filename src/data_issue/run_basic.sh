4cce964c03a11e449007a676ea2c4f87d18a56b7
KiyoshiKAWASAKI <meitanteirie@yahoo.co.jp>
2017-04-16 17:38:04 -0400

    fix bugs in shell

--- src/data_issue/run_basic.sh
+++ src/data_issue/run_basic.sh
@@ -3,12 +3,12 @@ filename="$1"
 
 mkdir basic_out
 
-for ((i=0; i<52; i++)); do
+for ((i=0; i<53; i++)); do
 	hadoop fs -rm -r "basic_$i.out"
 done
 
 spark-submit basic_statistics_info.py "$filename"
-for ((i=0; i<52; i++)); do
+for ((i=0; i<53; i++)); do
 	hadoop fs -getmerge "basic_$i.out" "basic_$i.txt"
 	mv "basic_$i.txt" basic_out/
 	hadoop fs -rm -r "basic_$i.out"
