db966a10d9724ab9508b5c93a7c9c46a43c266e2
KiyoshiKAWASAKI <meitanteirie@yahoo.co.jp>
2017-04-16 18:01:39 -0400

    fix bugs in shells

--- src/data_issue/run_mv.sh
+++ src/data_issue/run_mv.sh
@@ -3,13 +3,13 @@ filename="$1"
 
 mkdir mv_out
 
-for ((i=0; i<53; i++)); do
+for ((i=0; i<52; i++)); do
 	hadoop fs -rm -r "mv_$i.out"
 done
 
 spark-submit missing_values.py "$filename"
 
-for ((i=0; i<53; i++)); do
+for ((i=0; i<52; i++)); do
 	hadoop fs -getmerge "mv_$i.out" "mv_$i.txt"
 	mv "mv_$i.txt" mv_out/
 	hadoop fs -rm -r "mv_$i.out"
--- src/data_issue/run_valid.sh
+++ src/data_issue/run_valid.sh
@@ -2,7 +2,7 @@ filename="$1"
 
 mkdir validation
 
-for ((i=0; i<53; i++)); do
+for ((i=0; i<52; i++)); do
 	hadoop fs -rm -r "valid_$i.out"
 done
 
@@ -10,7 +10,7 @@ spark-submit validate_index0_to_16.py "$filename"
 spark-submit validate_index17_to_33.py "$filename"
 spark-submit validate_index34_to_51.py "$filename"
 
-for ((i=0; i<53; i++)); do
+for ((i=0; i<52; i++)); do
 	hadoop fs -getmerge "valid_$i.out" "valid_$i.txt"
 	mv "valid_$i.txt" validation
 	hadoop fs -rm -r "valid_$i.out"
