4218b011541255b5b7f8eba9f444a2236afe1dfa
cipta <ciphwn@gmail.com>
2017-04-15 22:38:17 -0400

    turn parser into a multifile thing

--- deliverable1/column_analysis.py
+++ deliverable1/column_analysis.py
@@ -1,6 +1,7 @@
 import csv
 import re
 import argparse
+import os
 import random
 from cStringIO import StringIO
 from operator import add
@@ -10,8 +11,12 @@ import locations
 from pyspark import SparkContext
 
 parser = argparse.ArgumentParser(description='Big Data Taxi Parser')
-parser.add_argument('--file_path', type=str, default='yellow_tripdata_test.csv',
-                    help='location of csv file in HDFS.')
+parser.add_argument('--input_dir', type=str, default='test_inputs.txt',
+                    help='location of csv files in HDFS.')
+parser.add_argument('--min_year', type=int, default=2013,
+                    help='first year to begin parsing.')
+parser.add_argument('--max_year', type=int, default=2016,
+                    help='last year to begin parsing.')
 parser.add_argument('--min_partitions', type=int, default=5,
                     help='minimum number of data partitions when loading')
 parser.add_argument('--save_path', type=str, default='./',
@@ -27,14 +32,24 @@ args = parser.parse_args()
 
 # Find out which year/month we're dealing with.
 file_month_re = re.compile('(\d\d\d\d)-(\d\d)')
-filename_match = file_month_re.search(args.file_path)
-expected_year = None
-expected_month = None
-if filename_match:
-    expected_year = int(filename_match.group(1))
-    expected_month = int(filename_match.group(2))
-    print('='*80 + '\n' + 'YEAR: {0}, MONTH: {1:02d}'.format(
-        expected_year, expected_month) + '\n' + '='*80)
+def read_file_path(filepath):
+    '''Reads the file path and determines the data range of this file.
+
+    Args:
+        filepath: a string (the filename), e.g. yellow_tripdata_2016-09.csv
+    Returns:
+        expected_year: expected calendar year of records in this file.
+        expected_month: expected calendar month of records in this file.
+        If no time information can be inferred, both will return None.'''
+    filename_match = file_month_re.search(filepath)
+    expected_year = None
+    expected_month = None
+    if filename_match:
+        expected_year = int(filename_match.group(1))
+        expected_month = int(filename_match.group(2))
+        # print('='*80 + '\n' + 'YEAR: {0}, MONTH: {1:02d}'.format(
+        #     expected_year, expected_month) + '\n' + '='*80)
+    return expected_year, expected_month
 
 
 def to_csv(l):
@@ -287,6 +302,8 @@ def parse_16_total(x):
 ################################################################################
 
 def keep_valid(row):
+    '''For a given data row, choose whether to print out the data. Args:
+        row: A tuple (value, base_type, semantic_type, label, count)'''
     try:
         # Row: (value, base_type, semantic_type, valid_invalid)
         if row[3] == 'VALID' and random.random() > args.keep_valid_rate:
@@ -298,40 +315,99 @@ def keep_valid(row):
         return True
 
 def csv_row_read(x):
+    '''Turns a CSV string (x) into a list of columns.'''
     return next(csv.reader([x]))
 
-def main():
-    # Keep as a list because dictionary iteration order is undefined.
+
+################################################################################
+
+def process_one_file(filepath):
+    '''Breaks a file into columns and does value tagging on each column.
+
+    Args:
+        filepath: string, where to get the file
+    Returns:
+        a list of CSV line RDDs, one for each column. They can either be
+        dumped or saved to a file.
+
+        Each column emits a tuple: (colname, RDD, invalid_rows) where the
+        second value are rows that don't contain this column.
+    '''
+    # This helps date column validation.
+    expected_year, expected_month = read_file_path(filepath)
+
+    # Common columns for all years.
     column_dict = {
         'VendorID': parse_0_vendor,
-        # Date time column for 2014.
-        'pickup_datetime': parse_1_pickup_datetime,
-        'dropoff_datetime': parse_2_dropoff_datetime,
-        # Date time column for 2015.
-        'tpep_pickup_datetime': parse_1_pickup_datetime,
-        'tpep_dropoff_datetime': parse_2_dropoff_datetime,
-        # Locations.
-        'PULocationID': parse_pu_location_id,
-        'DOLocationID': parse_do_location_id,
-        # Locations (pre-2016).
-        'pickup_longitude': locations.parse_longitude,
-        'pickup_latitude': locations.parse_latitude,
-        'dropoff_longitude': locations.parse_longitude,
-        'dropoff_latitude': locations.parse_latitude,
-        # 'passenger_count': parse_3_passenger_count,
-        # 'trip_distance': parse_4_trip_distance,
-        # 'RatecodeID': parse_5_rate_code,
-        # 'store_and_fwd_flag': parse_6_store_and_fwd,
-        # 'payment_type': parse_9_payment_type,
-        # 'fare_amount': parse_10_fare,
-        # 'extra': parse_11,
-        # 'mta_tax': parse_12,
-        # 'tip_amount': parse_13,
-        # 'tolls_amount': parse_14,
-        # 'improvement_surcharge': parse_15,
-        # 'total_amount': parse_16
+        'passenger_count': parse_3_passenger_count,
+        'trip_distance': parse_4_trip_distance,
+        'RatecodeID': parse_5_rate_code,
+        'store_and_fwd_flag': parse_6_store_and_fwd,
+        'payment_type': parse_9_payment_type,
+        'fare_amount': parse_10_fare,
+        'extra': parse_11,
+        'mta_tax': parse_12,
+        'tip_amount': parse_13,
+        'tolls_amount': parse_14,
+        'improvement_surcharge': parse_15,
+        'total_amount': parse_16
     }
 
+    # Date column changes from 2015.
+    if expected_year < 2015:
+        column_dict['pickup_datetime'] =
+            pickup_datetime_parser(expected_year, expected_month)
+        column_dict['dropoff_datetime'] =
+            pickup_datetime_parser(expected_year, expected_month)
+    else:
+        column_dict['tpep_pickup_datetime'] =
+            pickup_datetime_parser(expected_year, expected_month)
+        column_dict['tpep_dropoff_datetime'] =
+            pickup_datetime_parser(expected_year, expected_month)
+
+    # Locations are different before/after 2016.
+    if expected_year < 2016:
+        column_dict['pickup_longitude'] = locations.parse_longitude
+        column_dict['pickup_latitude'] = locations.parse_latitude
+        column_dict['dropoff_longitude'] = locations.parse_longitude
+        column_dict['dropoff_latitude'] = locations.parse_latitude
+    else:
+        column_dict['PULocationId'] = parse_pu_location_id
+        column_dict['DOLocationId'] = parse_do_location_id
+
+    # Load the text file and split out the header.
+    rdd = sc.textFile(file_path, minPartitions=args.min_partitions)
+    header_line = rdd.first()
+    header = csv_row_read(header_line)
+    # Filter empty lines and the header.
+    rdd = rdd.filter(lambda row: len(row) > 0 and row != header_line)
+
+    # Split each row into columns.
+    all_rows = rdd.map(lambda x: csv_row_read(x))
+
+    # Split off each column and analyze.
+    column_results = []
+    for i, col in enumerate(header):
+        col = col.strip()
+        parse_func = column_dict.get(col, None)
+        if parse_func is None:
+            print('{0}: Don\'t know how to read column: {1}'.format(
+                filepath, col))
+            continue
+
+        # Get rows that have the containing column.
+        rows = all_rows.filter(lambda col: len(col) > i)
+        rows_missing_this_col = all_rows.filter(lambda col: len(col) <= i)
+        # Get all unique values and analyze.
+        values = rows.map(lambda row: (row[i], 1)).reduceByKey(add)
+        values = values.map(parse_func)
+
+        column_results.append((col, values, rows_missing_this_col))
+    return column_results
+
+################################################################################
+
+def main():
     sc = SparkContext()
 
     # If your code calls out to other python files, add them here.
@@ -339,42 +415,29 @@ def main():
     sc.addPyFile('locations.py')
     sc.addFile('taxi_zone_lookup.csv')
 
-    rdd = sc.textFile(args.file_path, minPartitions=args.min_partitions)
-    header_line = rdd.first()
-    header = csv_row_read(header_line)
-    rdd = rdd.filter(lambda row: row != header_line)
-    for i, col in enumerate(header):
-        col = col.strip()
-        parse_func = column_dict.get(col, None)
-        if parse_func is None:
-            print('Don\'t know how to read column: {0}'.format(col))
-            continue
+    filename_format = 'yellow_tripdata_{0}-{1:02d}.csv'
+    for year in range(args.min_year, args.max_year + 1):
+        for month in range(1, 13):
+            filename = filename_format.format(year, month)
+            filepath = os.path.join(args.input_dir, filename)
+            print('Getting:', filepath)
 
-        # Split into columns.
-        all_rows = rdd.map(lambda x: csv_row_read(x))
-        # Get rows that have the containing column.
-        rows = all_rows.filter(lambda col: len(col) > i)
-        # What about rows that don't have enough columns.
-        rows_non = all_rows.filter(lambda col: len(col) <= i)
-
-        values = rows.map(lambda row: (row[i], 1)).reduceByKey(add)
         # Feed values RDD to a parser.
-        values = values.map(parse_func)
         # For each tuple returned by the parse_func, dup it to a csv
         # defined per column.
-        if values is not None:
-            values = values.filter(keep_valid).map(to_csv)
-            if args.dump:
-                print('Column: {0}'.format(col))
-                for row in values.collect():
-                    print(row)
-            else:
-                values.saveAsTextFile(
-                        args.save_path + '/{}.csv'.format(col))
-
-        # Dump some of the invalid rows.
-        for row in rows_non.collect():
-            print(row)
+        # if values is not None:
+        #     values = values.filter(keep_valid).map(to_csv)
+        #     if args.dump:
+        #         print('Column: {0}'.format(col))
+        #         for row in values.collect():
+        #             print(row)
+        #     else:
+        #         values.saveAsTextFile(
+        #                 args.save_path + '/{}.csv'.format(col))
+        #
+        # # Dump some of the invalid rows.
+        # for row in rows_non.collect():
+        #     print(row)
         
 
 if __name__ == '__main__':
