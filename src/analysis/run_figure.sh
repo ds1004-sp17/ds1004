b35da9a7f5526f5e15283d34be84191443ecece6
xiaodiu2010 <xiaodiu2010@gmail.com>
2017-04-16 16:47:12 -0400

    analysis

--- src/analysis/run_figure.sh
+++ src/analysis/run_figure.sh
@@ -1,3 +1,9 @@
+module purge 
+module load python/gnu/3.4.4
+export PYSPARK_PYTHON=/share/apps/python/3.4.4/bin/python
+export PYTHONHASHSEED=0
+export SPARK_YARN_USER_ENV=PYTHONHASHSEED=0
+
 filename="$1"
 
 spark-submit agency_dist_year.py $filename
