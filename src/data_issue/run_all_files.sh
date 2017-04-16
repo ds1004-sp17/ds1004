8f6705a8145364a52956ecb9cc7512acbb9032fd
KiyoshiKAWASAKI <meitanteirie@yahoo.co.jp>
2017-04-16 16:12:13 -0400

    fix bug

--- src/data_issue/run_all_files.sh
+++ src/data_issue/run_all_files.sh
@@ -1,46 +1,45 @@
-module purge 
-module load python/gnu/3.4.4
-export PYSPARK_PYTHON=/share/apps/python/3.4.4/bin/python
-export PYTHONHASHSEED=0
-export SPARK_YARN_USER_ENV=PYTHONHASHSEED=0
-
 # The input csv file
 filename="$1"
 
 mkdir basic_out
-spark-submit basic_statistics_info.py "$filename"
-for ((i=0; i<52; i++)); do
+spark-submit basic_statistics_info.py $filename
+for ((i=0; i<52; i++)); 
+do
 	hadoop fs -getmerge "basic_$i.out" "basic_$i.txt"
 	mv "basic_$i.txt" basic_out/
 	hadoop fs -rm -r "basic_$i.out"
 done
 
 mkdir mv_out
-spark-submit missing_values.py "$filename"
-for ((i=0; i<52; i++)); do
+spark-submit missing_values.py $filename
+for ((i=0; i<52; i++)); 
+do
 	hadoopfs -getmerge "mv_$i.out" "mv_$i.txt"
 	mv "mv_$i.txt" mv_out/
 	hadoopfs -rm -r "mv_$i.out"
 done
 
 mkdir validation
-spark-submit validate_index0_to_16.py "$filename"
-for ((i=0; i<17; i++)); do
+spark-submit validate_index0_to_16.py $filename
+for ((i=0; i<17; i++));
+do
 	hadoop fs -getmerge "valid_$i.out" "valid_$i.txt"
 	mv "valid_$i.txt" validation
 	hadoop fs -rm -r "valid_$i.out"
 done
 
-spark-submit validate_index17_to_33.py "$filename"
-for ((i=17; i<34; i++)); do
+spark-submit validate_index17_to_33.py $filename
+for ((i=17; i<34; i++));
+do
 	hadoop fs -getmerge "valid_$i.out" "valid_$i.txt"
 	mv "valid_$i.txt" validation
 	hadoop fs -rm -r "valid_$i.out"
 done
 
-spark-submit validate_index34_to_51.py "$filename"
-for ((i=34; i<52; i++)); do
-	hadoop fs -getmerge "_$i.out" "basic_$i.txt"
+spark-submit validate_index34_to_51.py $filename
+for ((i=34; i<52; i++));
+do
+	hadoop fs -getmerge "valid_$i.out" "basic_$i.txt"
 	mv "valid_$i.txt" validation
 	hadoop fs -rm -r "valid_$i.out"
 done
