01c8bf4d54f19cbe2de770ceaef67797162ad9ae
KiyoshiKAWASAKI <meitanteirie@yahoo.co.jp>
2017-04-16 14:54:38 -0400

    fix bugs

--- src/data_issue/run_all_files.sh
+++ src/data_issue/run_all_files.sh
@@ -10,37 +10,37 @@ filename="$1"
 mkdir basic_out
 spark-submit basic_statistics_info.py "$filename"
 for ((i=0; i<52; i++)); do
-	hfs -getmerge "basic_$i.out" "basic_$i.txt"
+	hadoop fs -getmerge "basic_$i.out" "basic_$i.txt"
 	mv "basic_$i.txt" basic_out/
-	hfs -rm -r "basic_$i.out"
+	hadoop fs -rm -r "basic_$i.out"
 done
 
 mkdir mv_out
 spark-submit missing_values.py "$filename"
 for ((i=0; i<52; i++)); do
-	hfs -getmerge "mv_$i.out" "mv_$i.txt"
+	hadoopfs -getmerge "mv_$i.out" "mv_$i.txt"
 	mv "mv_$i.txt" mv_out/
-	hfs -rm -r "mv_$i.out"
+	hadoopfs -rm -r "mv_$i.out"
 done
 
 mkdir validation
 spark-submit validate_index0_to_16.py "$filename"
 for ((i=0; i<17; i++)); do
-	hfs -getmerge "valid_$i.out" "valid_$i.txt"
+	hadoop fs -getmerge "valid_$i.out" "valid_$i.txt"
 	mv "valid_$i.txt" validation
-	hfs -rm -r "valid_$i.out"
+	hadoop fs -rm -r "valid_$i.out"
 done
 
 spark-submit validate_index17_to_33.py "$filename"
 for ((i=17; i<34; i++)); do
-	hfs -getmerge "valid_$i.out" "valid_$i.txt"
+	hadoop fs -getmerge "valid_$i.out" "valid_$i.txt"
 	mv "valid_$i.txt" validation
-	hfs -rm -r "valid_$i.out"
+	hadoop fs -rm -r "valid_$i.out"
 done
 
 spark-submit validate_index34_to_51.py "$filename"
 for ((i=34; i<52; i++)); do
-	hfs -getmerge "_$i.out" "basic_$i.txt"
+	hadoop fs -getmerge "_$i.out" "basic_$i.txt"
 	mv "valid_$i.txt" validation
-	hfs -rm -r "valid_$i.out"
+	hadoop fs -rm -r "valid_$i.out"
 done
