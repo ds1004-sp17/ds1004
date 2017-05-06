29607f137e6aa17bdd4a14b82db428ee72061b64
cipta <ciphwn@gmail.com>
2017-05-06 12:44:25 -0400

    locations

--- deliverable2/extract_monthly_stats.py
+++ deliverable2/extract_monthly_stats.py
@@ -9,6 +9,7 @@ from cStringIO import StringIO
 from operator import add
 import datetimes
 from pyspark import SparkContext, SparkConf
+from rtree import Index
 
 pu_datetime_index = 1
 do_datetime_index = 2
@@ -18,9 +19,8 @@ do_location_id_index = 8
 parser = argparse.ArgumentParser(description='Taxi net traffic.')
 parser.add_argument('--input_dir', type=str, default='public/taxis/',
                     help='location of csv files in HDFS.')
-parser.add_argument('--start_month', type=int, default=7, help='start month.')
-parser.add_argument('--end_month', type=int, default=12, help='end month.')
-parser.add_argument('--save_path', type=str, default='./net_traffic.csv',
+parser.add_argument('--month', type=int, default=7, help='month to process.')
+parser.add_argument('--save_path', type=str, default='./monthly_stats/',
                     help='directory in HDFS to save files to.')
 parser.add_argument('--loglevel', type=str, default='WARN',
                     help='log verbosity.')
@@ -84,45 +84,92 @@ def process(string):
     # Feed to a flatMap.
     return [(pu_key, (1, 0)), (do_key, (0, 1))]
 
+min_lon, max_lon = -74.30, -73.65
+min_lat, max_lat = 40.45, 40.95
+def in_range(lon, lat):
+    return lon >= min_lon and lon <= max_lon and \
+            lat >= min_lat and lat <= max_lat
 
-def tuple_add(x, y):
-    x0, x1 = x
-    y0, y1 = y
-    return (x0 + y0, x1 + y1)
+index = index.Index()
+for i, t in enumerate(taxi_zones_shapes):
+    index.insert(i, t.shape.bbox)
 
+def get_location_id(lon, lat):
+    lon = float(lon)
+    lat = float(lat)
+    pt = Point(lon, lat)
+    for ic in idx.intersection(pt)
+        c = taxi_zones_shapes[ic]
+        if c.shape.contains(pt):
+            return c.properties['LocationID']
 
-def format_add_result(pair):
-    key, (pu_count, do_count) = pair
-    row = list(key)
-    row.append(pu_count)
-    row.append(do_count)
-    return to_csv(row)
+def to_location_ids(header, row):
+    '''Converts a (lat, lon) formatted file into location ID.'''
+    p_lat_idx = header.indexof('pickup_latitude')
+    p_lon_idx = header.indexof('pickup_longitude')
+    d_lat_idx = header.indexof('dropoff_latitude')
+    d_lon_idx = header.indexof('dropoff_longitude')
 
-filepaths = [
+    try:
+        row.append(get_location_id(row[p_lon_idx], row[p_lat_idx]))
+        row.append(get_location_id(row[d_lon_idx], row[d_lat_idx]))
+        return row
+    except:
+        return None
+
+def extract(h, row):
+    if row is None:
+        return None
+    try:
+        r = [row[h['pulocationid']],
+             row[h['dolocationid']],
+             row[h['passenger_count']],
+             row[h['trip_distance']],
+             row[h['payment_type']],
+             row[h['total_amount']]]
+        if 'pickup_datetime' in h:
+            r.append(row[h['pickup_datetime']])
+            r.append(row[h['dropoff_datetime']])
+        if 'tpep_pickup_datetime' in h:
+            r.append(row[h['tpep_pickup_datetime']])
+            r.append(row[h['tpep_dropoff_datetime']])
+        if 'lpep_pickup_datetime' in h:
+            r.append(row[h['lpep_pickup_datetime']])
+            r.append(row[h['lpep_dropoff_datetime']])
+    except:
+        return None
+
+filepath = \
     os.path.join(
-        args.input_dir, 'yellow_tripdata_2016-{:02d}.csv'.format(month))
-    for month in range(args.start_month, args.end_month + 1)
-]
+        args.input_dir,
+        'yellow_tripdata_2015-{:02d}.csv'.format(args.month))
 
 def main():
-    conf = SparkConf().setAppName('net_traffic')
+    conf = SparkConf().setAppName('extract_monthly_stats')
     sc = SparkContext()
     sc.setLogLevel(args.loglevel)
     sc.addPyFile('datetimes.py')
 
     print('-'*80 + '\n' + 'net traffic counter' + '\n' + '-'*80)
-    for filepath in filepaths:
-        print(filepath)
-
+    print(filepath)
     print('Save to:', args.save_path)
 
     not_null = lambda x: x is not None
-    all_data = sc.union([sc.textFile(x) for x in filepaths])
-    counts = all_data.flatMap(process).filter(not_null).reduceByKey(tuple_add)
-    counts\
-        .sortByKey()\
-        .map(format_add_result)\
-        .saveAsTextFile(args.save_path)
+    all_data = sc.textFile(filepath)
+    header_line = all_data.first().lower()
+    header = {col:idx for idx, col in enumerate(csv_row_read(header_line))}
+
+    # Detect if the location ID is present.
+    if 'pulocationid' in header and 'dolocationid' in header:
+        all_data = all_data.map(partial(extract, header))
+    else:
+        all_data = all_data.map(partial(to_location_ids, header))
+        header['pulocationid'] = len(header)
+        header['dolocationid'] = len(header)
+        all_data = all_data.map(partial(extract, header))
+
+    all_data = all_data.filter(not_null)
+    all_data.map(to_csv).saveAsTextFile(args.save_path)
 
 if __name__ == '__main__':
     main()
