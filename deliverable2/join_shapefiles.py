c47ec1437a2ef8c883bde637419969c0ed7316a1
cipta <ciphwn@gmail.com>
2017-05-07 02:58:29 -0400

    optimize and fix bugs

--- deliverable2/join_shapefiles.py
+++ deliverable2/join_shapefiles.py
@@ -7,15 +7,11 @@ import os
 from cStringIO import StringIO
 import json
 from shapely.geometry import mapping, shape
-from shapely.ops import cascaded_union
 from pyspark import SparkContext, SparkConf
 import itertools
 import networkx as nx
 
-pu_location_id_index = 0
-do_location_id_index = 1
-pu_datetime_index = 6
-do_datetime_index = 7
+threshold = 0.5
 
 parser = argparse.ArgumentParser(description='Join Taxi and Zillow shapefiles')
 parser.add_argument('--K', type=int, default=5, help='neighbors to consider.')
@@ -43,15 +39,17 @@ z_hoods = json.load(open(args.zillow_shapes_path))
 t_edges = json.load(open(args.taxi_edges_path))
 z_edges = json.load(open(args.zillow_edges_path))
 
-z_hoods['features'] = [f for f in z_hoods['features']
-                      if f['properties']['City'] == 'New York']
-
 class Neighborhood(object):
     def __init__(self, feature):
         super(Neighborhood, self).__init__()
         self.properties = feature['properties']
         self.geometry = feature['geometry']
         self.shape = shape(feature['geometry'])
+
+z_hoods_shapes = [Neighborhood(f)
+                  for f in z_hoods['features']
+                  if f['properties']['City'] == 'New York']
+taxi_zones_shapes = [Neighborhood(f) for f in taxi_zones['features']]
 
 # Fit them into graphs.
 T = nx.Graph()
@@ -61,6 +59,8 @@ Z = nx.Graph()
 Z.add_edges_from(z_edges)
 Z = nx.freeze(Z)
 
+regionid_to_z = {int(z.properties['RegionID']): z for z in z_hoods_shapes}
+locid_to_t = {int(t.properties['LocationID']): t for t in taxi_zones_shapes}
 
 def generate_connected_subgraphs(G):
     all_nodes = list(G)
@@ -89,11 +89,6 @@ def score_fn(shape_t, shape_z):
 
 def analyze(z0_idx):
     '''Analyze the neighbors around a Zillow point.'''
-    z_hoods_shapes = [Neighborhood(f) for f in z_hoods['features']]
-    taxi_zones_shapes = [Neighborhood(f) for f in taxi_zones['features']]
-    regionid_to_z = {int(z.properties['RegionID']): z for z in z_hoods_shapes}
-    locid_to_t = {int(t.properties['LocationID']): t for t in taxi_zones_shapes}
-
     z0 = z_hoods_shapes[z0_idx]
     clusters = []
 
@@ -110,6 +105,8 @@ def analyze(z0_idx):
     z_subgs = [list(x) for x in list(generate_connected_subgraphs(Znear))]
     t_subgs = [list(x) for x in list(generate_connected_subgraphs(Tnear))]
 
+    # Special pickled import.
+    from shapely.ops import cascaded_union
     for t_ids in t_subgs:
         t_shape = cascaded_union([locid_to_t[locid].shape for locid in t_ids])
         for z_ids in z_subgs:
@@ -123,6 +120,8 @@ def analyze(z0_idx):
 
 def format_cluster(cluster):
     ((t_ids, z_ids), score) = cluster
+    t_ids = [str(x) for x in t_ids]
+    z_ids = [str(x) for x in z_ids]
     return ('|'.join(t_ids), '|'.join(z_ids), score)
 
 def main():
@@ -135,12 +134,14 @@ def main():
     threshold = 0.5
     print('-'*80 + '\n' + 'graph join' + '\n' + '-'*80)
 
-    z_ids = [int(z['properties']['RegionID']) for z in z_hoods['features']]
-    zs = sc.parallelize(list(regionid_to_z.keys()))
+    print('len(Z):', len(z_hoods_shapes))
+    print('len(T):', len(taxi_zones_shapes))
+
+    zs = sc.parallelize(range(len(z_hoods_shapes)), numSlices=len(z_hoods_shapes))
     clusters = zs.flatMap(analyze).reduceByKey(max)
     # cluster: [ ( (t_ids, z_ids), score ) ]
 
-    print('Save to:', args.save_path)
+    print('Save to:', args.save_to)
     clusters.sortByKey()\
         .map(format_cluster)\
         .sortBy(lambda x: x[-1], ascending=False)\
